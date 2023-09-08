b870485f08d96edabebcbe6c7ef5c02065913286
328ke <86567902+328ke@users.noreply.github.com>
2023-09-08 05:48:30 +0300

    0-preprocessor 1-compiler 2-assembler 3-name 4-puts.c 5-printf.c 6-size.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+* main function where the code starts to run
+*
+* Returns zero
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
